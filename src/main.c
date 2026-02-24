15ff9a2ac3c76aa176f648919d2bff98740a5fc8
albdert <albertsen.lindberg@gmail.com>
2026-02-25 00:35:39 +0100

    UI, collision, gameloop

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <time.h>
 
-const int ROWS = 45;
-const int COLS = 30;
+const char *TITLE = "Snake";
+const int SCREEN_W = 600;
+const int SCREEN_H = 600;
 const int SIZE = 20;
+const int ROWS = SCREEN_W/SIZE;
+const int COLS = SCREEN_H/SIZE;
+
+const int FPS = 60;
+int tickrate = 6;
+int score = 0;
+
+// flags
+bool menu = true;
+bool dead = false;
+bool pause = false;
+bool quit = false;
 
 typedef enum {
     UP,
@@ -11,6 +25,21 @@ typedef enum {
     LEFT,
     RIGHT
 } Direction;
+
+typedef enum {
+    PLAY,
+    QUIT,
+    RESUME
+} ButtonID;
+
+typedef struct {
+    Rectangle bounds;
+    Color c1;
+    Color c2;
+    int border;
+    char *text;
+    ButtonID id;
+} Button;
 
 typedef struct {
     Rectangle segments[50];
@@ -26,12 +55,12 @@ void initGrid(Rectangle grid[][COLS]) {
     }
 }
 void drawGrid(Rectangle grid[][COLS]) {
+    //TODO: fikse rutenett eller liknende for bakgrunn
     for (int i=0; i<ROWS; i++) {
         for (int j=0; j<COLS; j++) {
             DrawRectangleRec(grid[i][j], BLUE);
         }
     }
- 
 }
 
 void initSnake(Snake *snake) {
@@ -57,13 +86,12 @@ void moveSnake(Snake *snake) {
         case RIGHT: snake->segments[0].x+=SIZE; break;
     }
 
-    if (snake->segments[0].y<0) { snake->segments[0].y=SIZE*COLS; }
-    if (snake->segments[0].y>600) { snake->segments[0].y=0; }
-    if (snake->segments[0].x<0) { snake->segments[0].x=SIZE*ROWS; }
-    if (snake->segments[0].x>900) { snake->segments[0].x=0; }
+    if (snake->segments[0].y<0) { snake->segments[0].y=SIZE*(COLS-1); }
+    if (snake->segments[0].y>=SCREEN_H) { snake->segments[0].y=0; }
+    if (snake->segments[0].x<0) { snake->segments[0].x=SIZE*(ROWS-1); }
+    if (snake->segments[0].x>=SCREEN_W) { snake->segments[0].x=0; }
 
-    for (int i=0; i<snake->len; i++) {
-        if (i==0) { continue; }
+    for (int i=1; i<snake->len; i++) {
         float newx = snake->segments[i].x;
         float newy = snake->segments[i].y;
         snake->segments[i].x = prevx;
@@ -102,8 +130,100 @@ void extendSnake(Snake *snake) {
     snake->len++;
 }
 
+void initFood(Rectangle *food) {
+    food->width = SIZE;
+    food->height = SIZE;
+    food->x = GetRandomValue(0, ROWS-1) * SIZE;
+    food->y = GetRandomValue(0, COLS-1) * SIZE;
+}
+void moveFood(Rectangle *food) {
+    int rndx = GetRandomValue(0, ROWS-1);
+    int rndy = GetRandomValue(0, COLS-1);
+    food->x = rndx*SIZE;
+    food->y = rndy*SIZE;
+}
+void drawFood(Rectangle *food) {
+    Vector2 pos = {food->x+(SIZE/2), food->y+(SIZE/2)};
+    DrawCircleV(pos, SIZE/2, RED);
+}
+void checkFood(Snake *snake, Rectangle *food) {
+    Rectangle head = snake->segments[0];
+    if (CheckCollisionRecs(head, *food)) {
+        extendSnake(snake);
+        moveFood(food);
+    }
+}
+
+void initButtons(Button buttons[]) {
+    int offset = SIZE/2;
+
+    Button play = {
+        .bounds = {
+            (SCREEN_W/2)-(SIZE*4),
+            (SCREEN_H/2)-(SIZE*4),
+            SIZE*7,
+            SIZE*2
+        }, 
+        .c1 = GRAY,
+        .c2 = DARKGRAY,
+        .border = 2,
+        .text = "Play [>]",
+        .id = PLAY
+    };
+
+    Button resume = play;
+    resume.text = "Resume [>]";
+    resume.id = RESUME;
+
+    Button quit = play;
+    quit.bounds.y = (SCREEN_H/2)-(SIZE*2)+offset;
+    quit.text = "Quit [x]";
+    quit.id = QUIT;
+
+    buttons[PLAY] = play;
+    buttons[QUIT] = quit;
+    buttons[RESUME] = resume;
+}
+void drawButtons(Button buttons[], int indexes[], int n) {
+    for (int i=0; i<n; i++) {
+        Button *b = &(buttons[indexes[i]]);
+        DrawRectangleRec(b->bounds, b->c1);
+        DrawRectangleLinesEx(b->bounds, b->border, b->c2);
+        DrawText(b->text, b->bounds.x+SIZE, b->bounds.y+SIZE/2, SIZE,b->c2);
+    }
+}
+
+void initGame(Button b[], Rectangle g[][COLS], Snake *s, Rectangle *f) {
+    initButtons(b);
+    initGrid(g);
+    initSnake(s);
+    initFood(f);
+}
+void drawGame(Rectangle g[][COLS], Snake *s, Rectangle *f) {
+    ClearBackground(RAYWHITE);
+    drawGrid(g);
+    drawFood(f);
+    drawSnake(s);
+}
+
+void mainmenu(Button b[], int i[], int n, char *text) {
+    int textOffsetX = MeasureText(text, SIZE*4)/2;
+    ClearBackground(RAYWHITE);
+    DrawText(text, (SCREEN_W/2)-(textOffsetX), (SCREEN_H/6), SIZE*4, DARKGRAY);
+    drawButtons(b, i, 2);
+}
+void pausemenu(Button b[], int i[], int n) {
+    int textOffsetX = MeasureText("Paused", SIZE*4)/2;
+    DrawRectangle(0,0,SCREEN_W,SCREEN_H, (Color){255,255,255,128});
+    DrawText("Paused", (SCREEN_W/2)-(textOffsetX), (SCREEN_H/6), SIZE*4, DARKGRAY);
+    drawButtons(b, i, n); 
+}
+
+void tick(int *tickCnt) {
+    (*tickCnt) = ((*tickCnt)+1) % tickrate;
+}
 void handleInput(Snake *snake) {
-    switch (GetKeyPressed()) {
+    switch ( GetKeyPressed() ) {
     case KEY_W: 
         if (snake->dir==DOWN) { break; }
         snake->dir = UP; break;
@@ -116,34 +236,92 @@ void handleInput(Snake *snake) {
     case KEY_D: 
         if (snake->dir==LEFT) { break; }
         snake->dir = RIGHT; break;
-
+    case KEY_ESCAPE:
+        if (pause) { pause = false; }
+        else { pause = true; }
+        break;
     case KEY_SPACE:
         extendSnake(snake);
         break;
     default: break;
     }
 }
-
-int main(void) {
-    Rectangle grid[ROWS][COLS];
-    Snake snake;
-
-    InitWindow(900, 600, "Hello world!");
-    initGrid(grid);
-    initSnake(&snake);
-
-    SetTargetFPS(15);
-    while ( !WindowShouldClose() ) {
-        handleInput(&snake);
-        moveSnake(&snake);
-
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            drawGrid(grid);
-            drawSnake(&snake);
-       EndDrawing();
+void handleButtons(Button buttons[], int indexes[], int n) {
+    Vector2 mouse = GetMousePosition();
+    int action;
+    for (int i=0; i<n; i++) {
+        Button *b = &(buttons[indexes[i]]); 
+        if (CheckCollisionPointRec(mouse, b->bounds)) {
+            if (IsMouseButtonPressed(0)) { action = b->id; }
+            b->c1 = LIGHTGRAY;
+        } else { b->c1 = GRAY; }
     }
 
+    switch (action) {
+    case PLAY: menu=false; dead=false; break;
+    case QUIT: quit=true; break;
+    case RESUME: pause = false; break;
+    default: break;
+    }
+}
+void checkCollision(Snake *snake) {
+    Rectangle head = snake->segments[0];
+    for (int i=1; i<snake->len; i++) {
+        if (CheckCollisionRecs(head, snake->segments[i])) {
+            dead = true;
+            menu = true;
+        }
+    }
+}
+
+int main(void) {
+    Button buttons[8];
+    Rectangle grid[ROWS][COLS];
+    Rectangle food;
+    Snake snake;
+
+    int menuButtons[]  = {PLAY, QUIT};
+    int pauseButtons[] = {RESUME, QUIT};
+    char *menuText = (char*)TITLE;
+
+    int ticks = 0;
+
+    InitWindow(SCREEN_W, SCREEN_H, TITLE);
+    SetExitKey(KEY_NULL);
+    SetRandomSeed(time(NULL));
+    initGame(buttons, grid, &snake, &food);
+
+    SetTargetFPS(FPS);
+    while ( !WindowShouldClose() && !quit ) {
+        if (menu) {
+            if (dead) { menuText = "Game Over"; }
+            BeginDrawing();
+            mainmenu(buttons, menuButtons, 2, menuText);
+            EndDrawing();
+            handleButtons(buttons, menuButtons, 2);
+            continue;
+        }
+        if (pause) {
+            BeginDrawing();
+            drawGame(grid, &snake, &food);
+            pausemenu(buttons, pauseButtons, 2);
+            EndDrawing();
+            handleButtons(buttons, pauseButtons, 2);
+            continue;
+        }
+
+        handleInput(&snake);
+        tick(&ticks);
+        if (ticks==0) {  
+            moveSnake(&snake);
+            checkFood(&snake, &food);
+            checkCollision(&snake);
+        }
+
+        BeginDrawing();
+        drawGame(grid, &snake, &food);
+        EndDrawing();
+    }
     CloseWindow();
     return 0;
 }
